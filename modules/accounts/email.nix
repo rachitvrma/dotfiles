4b97ba8cb00aafa6f7e07f2805f48c936bf1c974
Rachit Kumar Verma <rachitverma1122@gmail.com>
2026-08-31 16:17:28 +0530

    aerc: add notification method

--- modules/accounts/email.nix
+++ modules/accounts/email.nix
@@ -12,6 +12,7 @@
     home.packages = with pkgs; [
       # Used in image rendering in aerc
       chafa
+      libnotify # For notification in aerc
     ];
     programs = {
       aerc = {
@@ -30,6 +31,9 @@
             "text/plain" = "colorize"; # https://aerc-docs.com/ecosystem/w3m/
             # Image preview
             "image/*" = "chafa -f kitty -s $(tput cols)x$(tput lines) -";
+          };
+          hooks = {
+            mail-received = ''notify-send "New mail from $AERC_FROM_NAME" "$AERC_SUBJECT"'';
           };
           openers = {
             "image/*" = "xdg-open {}";
