c2fd88c9e082acc1dda28286217094847115f689
Rachit Kumar Verma <rachitverma1122@gmail.com>
2026-09-03 00:42:41 +0530

    yazi: move init.lua to fennel.

--- modules/yazi/init.lua
+++ modules/yazi/init.lua
@@ -1,13 +1,20 @@
-function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
-	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = os.date("%b %d  %Y", time)
-	end
-
-	local size = self._file:size()
-	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+-- [nfnl] init.fnl
+Linemode.size_and_mtime = function(self)
+  local time = math.floor((self._file.cha.mtime or 0))
+  if (time == 0) then
+    time = ""
+  elseif (os.date("%Y", time) == os.date("%Y")) then
+    time = os.date("%b %d %H:%M", time)
+  else
+    time = os.date("%b %d  %Y", time)
+  end
+  local size = self._file:size()
+  local _2_
+  if size then
+    _2_ = ya.readable_size(size)
+  else
+    _2_ = "-"
+  end
+  return string.format("%s %s", _2_, time)
 end
+return Linemode.size_and_mtime
